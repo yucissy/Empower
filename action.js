1f77275ad2c6c8570c2249f5b90febdc3ee46c03
yucissy <cissy_yu@brown.edu>
2015-11-06 21:24:31 -0500

    added auto password and user and submit capability

--- action.js
+++ action.js
@@ -1,5 +1,16 @@
-var myPassword = '123456';
+if (document.getElementById('password')) {
+	var myUsername = 'empowered_user';
+	var myPassword = '123456';
 
-var passwordField = document.getElementById('password');
+	var userField = document.getElementById('username');
+	var passwordField = document.getElementById('password');
 
-passwordField.value = myPassword;
+	passwordField.value = myPassword;
+	userField.value = myUsername;
+
+	var scriptNode = document.createElement('script');
+	scriptNode.textContent = "javascript:prepareSubmit();";
+
+	document.body.appendChild(scriptNode);
+}
+
