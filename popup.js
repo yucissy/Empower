10d02b2e57bc5b7e66e6e964f3c34abe6ae576f6
Jeremy Liu <jeremy.liu@yale.edu>
2015-11-07 02:05:29 -0500

    Messed with Parse

--- popup.js
+++ popup.js
@@ -1,32 +1,40 @@
 
 window.onload = function() {
-	Parse.initialize("TR0PpcB3RW3yejpUHTywnCtWHZ0M44wJTMH8mHMe", "7DBkXfxGDhsFsw29EACSBLlXlNnWomduRWj3YOoQ");
 
-// document.getElementById("login").onclick = function(event){
+  document.getElementById("login").onclick = function(event){
+
+  Parse.initialize("TR0PpcB3RW3yejpUHTywnCtWHZ0M44wJTMH8mHMe", "7DBkXfxGDhsFsw29EACSBLlXlNnWomduRWj3YOoQ");
 //     	event.preventDefault();
 //     	var name = document.getElementById("username").value;
 //     	var pass = document.getElementById("password", pass);
-//     	window.location.href="give_permit.html";
 //     };
-    
+  event.preventDefault();
+  var name = document.getElementById("username").value;
+  var pass = document.getElementById("password").value;
+
+  alert(name);
+  alert(pass);
+
 	var user = new Parse.User();
-	user.set("username", "Lexi");
-	user.set("password", "111");
-	user.set("email", "cliffweitzman@gmail.com");
+	user.set("username", name);
+	user.set("password", pass);
+	//user.set("email", "test@gmail.com");
 
 	// other fields can be set just like with Parse.Object
-	user.set("phone", "415-392-0202");
+	//user.set("phone", "415-392-0202");
 
 	user.signUp(null, {
 	  success: function(user) {
 		// Hooray! Let them use the app now.
 	  },
 	  error: function(user, error) {
-		// Show the error message somewhere and let the user try again.
-		alert("Error: " + error.code + " " + error.message);
+		  // Show the error message somewhere and let the user try again.
+		  alert("Error: " + error.code + " " + error.message);
 	  }
 	});
-    
 
+  window.location.href="give_permit.html";
+    
+};
 }
 
