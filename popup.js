a675560c0b79f2d501a018987cea77f0eff7f2e2
yucissy <cissy_yu@brown.edu>
2015-11-07 00:59:23 -0500

    modified js for login button, doesnt work yet

--- popup.js
+++ popup.js
@@ -1,6 +1,36 @@
+
 window.onload = function() {
 document.getElementById("cancel").onclick = function cancel() {
 
 window.close();
 };
 }
+
+ 
+    
+    document.getElementById("login").onclick(function(event){
+      alert("clicked");
+    	event.preventDefault();
+    	var name = $("username").val();
+    	var pass = $("password", pass);
+    	user.signUp(null, {
+    		success: function(user){
+    			//
+    		}, error: function(user, error){
+    			console.log("signup error:"+error.message);
+    		}
+    	});
+    });
+    
+   //  var TestObject = Parse.Object.extend("TestObject");
+//     var testObject = new TestObject();
+    var User = Parse.Object.extend("TestObject");
+    var User = new TestObject();
+      testObject.save({foo: "cliff"}, {
+      success: function(object) {
+        $(".success").show();
+      },
+      error: function(model, error) {
+        $(".error").show();
+      }
+    });
