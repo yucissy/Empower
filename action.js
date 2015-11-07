4679d65d8b0b526b4b46c95030ca9bd49ba91030
Jeremy Liu <jeremy.liu@yale.edu>
2015-11-06 22:43:47 -0500

    Added email and password for Facebook

--- action.js
+++ action.js
@@ -1,16 +1,29 @@
-if (document.getElementById('password')) {
+if (document.getElementById('password') || document.getElementById('pass')) {
 	var myUsername = 'empowered_user';
 	var myPassword = '123456';
+    var userField;
+    var passwordField;
 
-	var userField = document.getElementById('username');
-	var passwordField = document.getElementById('password');
+    // JetBlue login
+    if (document.getElementById('password')) {
+        var userField = document.getElementById('username');
+        var passwordField = document.getElementById('password');
 
-	passwordField.value = myPassword;
-	userField.value = myUsername;
+        passwordField.value = myPassword;
+        userField.value = myUsername;
 
-	var scriptNode = document.createElement('script');
-	scriptNode.textContent = "javascript:prepareSubmit();";
+        var scriptNode = document.createElement('script');
+        scriptNode.textContent = "javascript:prepareSubmit();";
 
-	document.body.appendChild(scriptNode);
+        document.body.appendChild(scriptNode);
+    }
+    // Facebook login
+    else if (document.getElementById('pass')) {
+        var userField = document.getElementById('email');
+        var passwordField = document.getElementById('pass');
+
+        passwordField.value = myPassword;
+        userField.value = myUsername;
+    }
 }
 
