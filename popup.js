a3657c93895b42f9b7bba96e7d78fd8de7d832a9
yucissy <cissy_yu@brown.edu>
2015-11-06 22:30:07 -0500

    finished first 2 user pages

--- popup.js
+++ popup.js
@@ -1,7 +1,6 @@
 window.onload = function() {
-document.getElementById("login").onclick = function login() {
-chrome.browserAction.setPopup( {
-	popup: 'give_permit.html'
-});
+document.getElementById("cancel").onclick = function cancel() {
+
+window.close();
 };
 }
