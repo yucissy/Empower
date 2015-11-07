1037e55d248d27693b13daafa336f444e5b24d93
Jeremy Liu <jeremy.liu@yale.edu>
2015-11-07 12:29:05 -0500

    Fixed signin, added filler signup.html, switched parse.js to version 1.6.7

--- popup.js
+++ popup.js
@@ -3,22 +3,21 @@ window.onload = function() {
   Parse.initialize("TR0PpcB3RW3yejpUHTywnCtWHZ0M44wJTMH8mHMe", "7DBkXfxGDhsFsw29EACSBLlXlNnWomduRWj3YOoQ");
 
   document.getElementById("login").onclick = function(event){
-    event.preventDefault();
+  event.preventDefault();
 
 	var name = document.getElementById("username").value;
 	var pass = document.getElementById("password").value;
 	
-	Parse.User.logIn("ctest1", "111", {
-	success: function(user) {
-	alert("hey");
-		
-	window.location.href="give_permit.html";
-	},
-	error: function(user, error) {
-	alert("failed");
-	}
-	});
+	Parse.User.logIn(name, pass, {
+	  success: function(user) {
+		  window.location.href="standin.html";
+	  },
+	  error: function(user, error) {
+	    alert("failed");
+	  }
+  });
 
 };
+
 }
 
