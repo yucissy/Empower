3b88c03383f700954aeb1f778c660377e160e155
Jeremy Liu <jeremy.liu@yale.edu>
2015-11-07 10:41:01 -0500

    Fixed Parse signUp

--- popup.js
+++ popup.js
@@ -1,39 +1,29 @@
 
 window.onload = function() {
+  Parse.initialize("TR0PpcB3RW3yejpUHTywnCtWHZ0M44wJTMH8mHMe", "7DBkXfxGDhsFsw29EACSBLlXlNnWomduRWj3YOoQ");
 
   document.getElementById("login").onclick = function(event){
+    event.preventDefault();
 
-  Parse.initialize("TR0PpcB3RW3yejpUHTywnCtWHZ0M44wJTMH8mHMe", "7DBkXfxGDhsFsw29EACSBLlXlNnWomduRWj3YOoQ");
-//     	event.preventDefault();
-//     	var name = document.getElementById("username").value;
-//     	var pass = document.getElementById("password", pass);
-//     };
-  event.preventDefault();
   var name = document.getElementById("username").value;
   var pass = document.getElementById("password").value;
-
-  alert(name);
-  alert(pass);
 
 	var user = new Parse.User();
 	user.set("username", name);
 	user.set("password", pass);
 	//user.set("email", "test@gmail.com");
-
-	// other fields can be set just like with Parse.Object
 	//user.set("phone", "415-392-0202");
 
 	user.signUp(null, {
 	  success: function(user) {
-		// Hooray! Let them use the app now.
+      // Navigate to permissions page
+      window.location.href="give_permit.html";
 	  },
 	  error: function(user, error) {
 		  // Show the error message somewhere and let the user try again.
 		  alert("Error: " + error.code + " " + error.message);
 	  }
 	});
-
-  window.location.href="give_permit.html";
     
 };
 }
